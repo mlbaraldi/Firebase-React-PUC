1bc41dc5426c6dfaccd910bb67ae342416503557
Marcelo Baraldi <48130176+mlbaraldi@users.noreply.github.com>
2022-04-17 01:16:49 -0300

    removendo dados do firebase

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore, collection } from "@firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyByo00Jm131MjSkIADGy1Hu9wsQtIDpaBU",
-  authDomain: "fir-pucpr-f6b89.firebaseapp.com",
-  projectId: "fir-pucpr-f6b89",
-  storageBucket: "fir-pucpr-f6b89.appspot.com",
-  messagingSenderId: "831706321518",
-  appId: "1:831706321518:web:9557e95ea706b1288ecc7f",
-  measurementId: "G-DGQDMXZTP6"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
